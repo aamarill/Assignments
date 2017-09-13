9be52d240dc4c80e2fc7f9be1820a19a009a928c
Adan Amarillas <aamarill.engr@gmail.com>
2017-09-13 14:08:28 -0700

    Add files via upload

---
+++ Assignment_13.js
@@ -0,0 +1,17 @@
+
+//Open an new JS Bin and create an function expression, a function declaration,
+// and an object. Save the JS Bin and submit the link to your mentor.
+
+var functionExpression = function(a,b){
+  return a+b
+}
+
+function functionDeclaration(a,b){
+  return a+b
+}
+
+var profile = {
+  name: 'Adan Amarillas',
+  age: 27,
+  hobbies: ['soccer','food', 'travel'],
+  };
